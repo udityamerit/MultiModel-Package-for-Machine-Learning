f2c12d2c8e237ca90a5827a451f1c9b0c223fd23
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-08-15 22:13:13 +0530

    adding the naive bayes algo

--- Model/multi_model.py
+++ Model/multi_model.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
@@ -79,13 +80,30 @@ def KNN_model(X, y):
     knn = classification_report(y_test, y_pred_logi_test)
     return knn
 
+def Naive_Bayes_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    sc = StandardScaler()
+    X_train_scaled = sc.fit_transform(X_train)
+    X_test_scaled = sc.transform(X_test)
+
+    model1 = GaussianNB()
+    model1.fit(X_train_scaled, y_train)
+    y_pred_logi_test = model1.predict(X_test_scaled)
+    logistic_accuracy = accuracy_score(y_test, y_pred_logi_test)
+    # print(f'KNN Accuracy: {logistic_accuracy*100} % ')
+
+    naive = classification_report(y_test, y_pred_logi_test)
+
+    return naive
+
 
 def run_all_model(X, y):
     models = [
         ("Regression model", Regression_model(X, y)),
         ("Support vector model", Support_vector_model(X, y)),
         ("Decision Tree model", DecisionTree_model(X, y)),
-        ("KNN model", KNN_model(X, y))
+        ("KNN model", KNN_model(X, y)),
+        ("Naive Bayes model", Naive_Bayes_model(X, y))
     ]
 
     for name, model in models:
